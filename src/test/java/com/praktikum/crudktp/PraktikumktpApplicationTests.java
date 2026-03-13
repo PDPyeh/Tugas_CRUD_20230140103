91e6ab3b61ec82909ce2f528b35758d9b58d1096
Pradipa Y <pdpyyo@gmail.com>
2026-03-13 13:35:54 +0700

    Initial commit

---
+++ src/test/java/com/praktikum/crudktp/PraktikumktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum.crudktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumktpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
