3d15ac64ec77a59d18f4ee5d423c37269cb1b26e
Pradipa Y <pdpyyo@gmail.com>
2026-03-13 13:56:06 +0700

    membuat UserRepository

---
+++ src/main/java/com/praktikum/crudktp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.praktikum.crudktp.repository;
+
+import com.praktikum.crudktp.model.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Ktp, String> {
+}
